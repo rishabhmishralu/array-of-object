a5673cd954c95ad14f221f709ed99ded1039d190
= <=>
2022-12-09 20:37:35 +0530

    add

---
+++ main.js
@@ -0,0 +1,41 @@
+let studentRecords =
+ [{name: 'John', id: 123, marks : 98 },
+{name: 'Baba', id: 101, marks : 23 },
+{name: 'yaga', id: 200, marks : 45 },
+{name: 'Wick', id: 115, marks : 75 } ] 
+
+// const result=studentRecords.map(index=>index.name.toUpperCase() )
+// console.log(result);
+
+// for( let i=0;i<studentRecords.length;i++)
+// console.log(result[i].name);
+
+// const result=studentRecords.filter((a,b)=>a.marks>=50&& a.id>120)
+// console.log(result);
+
+// const sum=studentRecords.map((a,b)=>{
+//     if(a.marks>50){
+//         console.log(a.name);
+//     }
+// })
+
+// #4
+// const result=studentRecords.reduce(((a,b)=>a+b.marks),0)
+// console.log(result);
+
+
+// 7
+
+// studentRecords.forEach((a)=>{
+//     if(a.marks<50){
+//         a.marks+=15
+//     }
+// })
+// let marks=0;
+// studentRecords.forEach((a)=>{
+//               if(a.marks>50){
+//                 marks+=a.marks
+//               }
+// })
+// console.log(marks);
+
